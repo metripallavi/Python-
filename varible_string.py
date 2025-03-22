958fbe9cf2ccb693672bea3f528aead38a7391bc
Pallavi Metri <metripallavi17@gmail.com>
2025-03-22 20:38:01 +0100

    worked on Datatypes

--- varible_string.py
+++ varible_string.py
@@ -1,4 +1,24 @@
+#Knowing Datatypes 
+#String #Int #Float #Boolean 
+
 First_name = "Pallavi"
 Last_name = "Metri"
 Full_name = (First_name + " " + Last_name)
 print(Full_name)
+
+age = 27   #Int datatype 
+age += 1
+print (age) 
+print ("My age is:"+ (str(age))) #String concatnation
+
+Height = 174.2 #Float Datatype
+print(Height)
+print (" my height is : " + str(Height) + "cm" ) 
+print (type(Height))
+
+#Boolean Datatype 
+human = True
+print(human)
+print("Are you a Human : ? "+ (str("yes")))
+print(type(human))
+
