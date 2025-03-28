fc57d7670c9a2a8ee83b877e115a9473da46973c
Pallavi Metri <metripallavi17@gmail.com>
2025-03-28 10:56:38 +0100

    Typecasting

---
+++ type_casting.py
@@ -0,0 +1,39 @@
+
+#typecasting
+
+x=1    #int
+y=2.0  #float
+z= "3" #String
+
+x = float(x)
+y = int(y)
+z = int(z)
+
+
+print(x)
+print(y)
+print(z*3)
+
+x = float(x) #typecasted x from int to float 
+y = float(y)
+z = float(z)
+
+print(x)
+print(y)
+print(z*3)
+
+x = str(x) #typecasted all to string
+y = str(y)
+z = str(z)
+
+print(" Thats a string typecast " + ( x ))
+print(y)
+print(z)
+
+
+x=1
+y=2.0
+z= "3"
+print("X is "+str(x))
+print("Y is "+str(y))
+print("Z is "+str(z))
