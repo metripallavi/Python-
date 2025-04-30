ea387d91196f05f1a9ee86254ba15711c98996e8
Pallavi Metri <metripallavi17@gmail.com>
2025-04-30 13:13:17 +0200

    learning multi dimentional lists 2D

---
+++ 2d_loops.py
@@ -0,0 +1,12 @@
+# 2D lists multi dim lists 
+
+
+drinks = ["coffee","soda","tea"]
+dinner = ["Pizza","ham","fries"]
+dessert =["cake","icecream"]
+
+food = [drinks,dinner,dessert]
+#print(food)
+print(food[0][1]) #prints oth list that is drinks and 1st element that is soda and same for below 
+print(food[1][2])
+print(food[2][1])
